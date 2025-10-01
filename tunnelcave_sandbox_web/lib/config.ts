917b1505bd76d48840d451a1a1f9df5dbae84f76
abrahamVado <abraham.vado@gmail.com>
2025-10-01 08:09:43 -0600

    Stream straight tunnel chunks seamlessly

--- tunnelcave_sandbox_web/lib/config.ts
+++ tunnelcave_sandbox_web/lib/config.ts
@@ -14,6 +14,7 @@ export interface SandboxParams {
   chunkLength: number;
   ringStep: number;
   tubeSides: number;
+  fieldType: "straight" | "curl";
   dirFreq: number;
   dirBlend: number;
   radiusBase: number;
@@ -36,6 +37,7 @@ export const defaultParams: SandboxParams = {
   chunkLength: 90,
   ringStep: 3,
   tubeSides: 20,
+  fieldType: "straight",
   dirFreq: 0.05,
   dirBlend: 0.65,
   radiusBase: 11,
@@ -48,7 +50,7 @@ export const defaultParams: SandboxParams = {
   joltEveryMeters: 140,
   joltStrength: 0.45,
   maxTurnPerStepRad: Math.PI / 6,
-  addEndCaps: true,
+  addEndCaps: false,
   profile: {
 
     baseScale: 1.3,
